6034c32f089179dbe9489dbde074dc23b9fde76b
ElBi21 <leonardo.biason@gmail.com>
2023-07-07 20:28:55 +0200

    Continued Lesson 06

--- src/Lesson_06/Package03/ClassThree.java
+++ src/Lesson_06/Package03/ClassThree.java
@@ -4,4 +4,9 @@ public class ClassThree extends ClassTwo{
     public void printSomething () {
         System.out.println("[PACKAGE 03] [CLASS 03] Something!");
     }
+
+    /* public void randomWord () {
+        super.printSomething();
+        this.printSomething();
+    } */
 }
--- src/Lesson_06/Package03/TestPackage03Class.java
+++ src/Lesson_06/Package03/TestPackage03Class.java
@@ -17,7 +17,17 @@ public class TestPackage03Class {
 
         ClassThree objectClassThree = new ClassThree();
 
-        // The randomWord() method has
-        objectClassThree.randomWord(); // this is dynamic, super is static
+        // The randomWord() method has two instructions inside:
+        //  - calls super.printSomething()
+        //  - calls this.printSomething()
+
+        objectClassThree.randomWord();
+
+        // We see that what was printed was the printSomething of ClassOne for the super.printSomething and the
+        // printSomething() of ClassThree. This happens because this is dynamic, while super is static. It's similar
+        // conceptually to what happens with the methods and the attributes in subclasses: super depends on the
+        // class where the method was called (in this case it's ClassTwo, because the method belongs to ClassTwo), while
+        // this refers to the object that called the method. If the method was in ClassThree then we would've seen the
+        // super print Class 02, while this would've printed Class 03
     }
 }
